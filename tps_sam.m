5b14a99c5b87c927c4c56f92b8f41b9c328edb53
egghaha601 <83370677+egghaha601@users.noreply.github.com>
2021-05-07 17:33:11 -0700

    Update tps_sam.m

--- tps_sam.m
+++ tps_sam.m
@@ -23,9 +23,9 @@
                     miss(i)=i;
                 end;
                 for i=1:gs
-                    AD(i)=i;%记录组的ID
+                    AD(i)=i;
                 end;
-                %---设置好了已知标签和未知标签，开始一次实验，多伦循环------------
+             %---------------------------------------------  
                 while(gs_num~=0) 
                     mk=zeros(1,gs);
                     for i=1:1:gs
@@ -36,7 +36,7 @@
                     else
                         f=gs_num;
                     end;
-                    AV2=zeros(1,f); %用来记录每次哈希映射使用的时隙
+                    AV2=zeros(1,f); 
                     lunshu=lunshu+1;
                     EV1=zeros(1,f);
                     EV11=zeros(1,f);
@@ -44,52 +44,51 @@
                     shanchu=zeros(1,gs);
                     k=1;
                     
-                    %------映射M标签
-                    result1=zeros(1,gs); %记录每个组ID的选择
+                    %------
+                    result1=zeros(1,gs); 
                     for i=1:1:gs_num
                         aaaa=unidrnd(f);
-                        result1(k)=aaaa; %将这些标签放到同一行中
+                        result1(k)=aaaa; 
                         k=k+1;
-                    end;
-                    
-                    %----做了两次哈希映射
-                    x=unique(result1);%不同元素个数
+                    end; 
+                    %----
+                    x=unique(result1);
                     y=(result1);
-                    [m1,n1]=hist(y,x); %m为n在Y中出现的次数
-                    % ------两次哈希映射
+                    [m1,n1]=hist(y,x); 
+                    % ------
                     for i=1:1:length(n1)
                         if n1(i)>0
                             EV11(n1(i))=m1(i);
                         end;
                         if m1(i)==1 && n1(i)>0
                             EV1(n1(i))=1;
-                        end;%EV中只有一个标签映射的时候才为1
+                        end;
                     end;
                     zxc=0;
                     for i=1:1:f
                         flag=0;
                         if EV11(i)==0||EV11(i)>1
-                            AV1(i)=0; %空时隙为0
+                            AV1(i)=0; 
                             
                         else if EV11(i)==1 &&AV2(i)==0
                                 a=find(result1==i);
-                                record_num=zeros(1,16); %记录每个标签映射的index的数量
-                                record_index=zeros(1,16); %记录每个标签映射的index
+                                record_num=zeros(1,16); 
+                                record_index=zeros(1,16); 
                                 for j=1:1:Arand(a)
                                     mm=rand(1,1);
-                                    sump=0; %累计概率
+                                    sump=0; %绱姒傜巼
                                     for js=1:1:16
                                         if mm>sump&&mm<=sump+(1/(2^js))
-                                            record_num(js)=record_num(js)+1; %记录对应的区间
-                                            record_index(js)=j; %记录对应的区间
+                                            record_num(js)=record_num(js)+1; 
+                                            record_index(js)=j; 
                                         end;
                                         sump=sump+(1/(2^js));
                                     end;
                                 end;
                                 uindex=find(record_num==1);
                                 if isempty(uindex)==0
-                                    if isempty(find(jicaiyang(AD(a),:)==record_index(uindex(1)),1))&&ppad(AD(a))<=s_num %之前没采样过
-                                        jicaiyang(AD(a),ppad(AD(a)))=record_index(uindex(1)); %记录采样的标签组别和系数
+                                    if isempty(find(jicaiyang(AD(a),:)==record_index(uindex(1)),1))&&ppad(AD(a))<=s_num 
+                                        jicaiyang(AD(a),ppad(AD(a)))=record_index(uindex(1)); 
                                         ppad(AD(a))=ppad(AD(a))+1;
                                     end;
                                     AV1(i)=1;
@@ -110,16 +109,15 @@
                         end;
                     end;
                     
-                    AD(shanchu(1:zxc))=[]; %这组有一个单时隙，则该组去除
+                    AD(shanchu(1:zxc))=[]; 
                     EVt=(f/96)*t_id;
-                    t2=t2+EVt;% 当jj=0时，为CCG算法，并计算时间
+                    t2=t2+EVt;
                     to=length(AD)*(sumshu/gs);
                 end;
                 
                 if sum(ppad)==((s_num+1)*gs)
-                    flag=1; %采集满了
+                    flag=1; 
                 end;  
-                %  t2=t2+(25*w+200)*gs+(4*37.45+200)*gs;%加上polling和采集的数据
-                t2=t2+t_inf*gs+(gs*(4)/96)*t_id;%加上polling和采集的数据
+                t2=t2+t_inf*gs+(gs*(4)/96)*t_id;
             end;
             tt=t2./1000000;
