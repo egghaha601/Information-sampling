d2a1ad8a244a1f64cfaeea1d3f93ad2f31eb9d45
egghaha601 <83370677+egghaha601@users.noreply.github.com>
2021-05-15 19:14:50 -0700

    Update coding.m

--- coding.m
+++ coding.m
@@ -3,12 +3,12 @@ clear all;
 clc;
  
 in='01000000';          
-pr=[0.875 0.125];        %各字符出现的概率  
+pr=[0.875 0.125];         
 temp=[0.0 0.875 1.0];  
 orignal=temp;  
  
 n=length(in);  
-%编码  
+%缂栫爜  
 for i=1:n  
     width=temp(3)-temp(1);  
     w=temp(1);  
@@ -29,7 +29,7 @@ end
 encode=0.78125;
 innum=encode;
 N=6;
-if (innum>1)|(N == 0)%判断输入的有效性
+if (innum>1)|(N == 0)
    disp('error!');
    return;
 end
@@ -37,12 +37,12 @@ count=0;
 tempnum=innum;
 record=zeros(1,N);
 while(N)
-   count=count+1;%长度小于N
+   count=count+1;
    if(count>N)
        N=0;
 %        return;
     end
-   tempnum=tempnum*2;%小数转换为二进制,乘2取整
+   tempnum=tempnum*2;
     if tempnum>1
        record(count)=1;
        tempnum=tempnum-1;
