09e6a6b7469babb6338e23fdf40a2e3f8f719146
egghaha601 <83370677+egghaha601@users.noreply.github.com>
2021-05-15 19:15:49 -0700

    Update edfsa_sam.m

--- edfsa_sam.m
+++ edfsa_sam.m
@@ -6,7 +6,7 @@ function[tt]=edfsa_sam(num_c,size_c,Sa,t_id, t_id2, t_inf)
             t2=0;
             B=zeros(1,T_all);
             for i=1:T_all
-                B(i)=i;    %每个标签有自己的编号(假设为96位)，其中前M个为已知标签，后N个为未知标签
+                B(i)=i;    
             end;
             while(T_all~=0)
                 count=0;
@@ -23,29 +23,29 @@ function[tt]=edfsa_sam(num_c,size_c,Sa,t_id, t_id2, t_inf)
                 
                 for i=1:1:T_all
                     aaaa=unidrnd(f);
-                    tag_sel(i)=aaaa;%产生1到f均匀分布的随机数，对应于各已知标签id映射的位置;
+                    tag_sel(i)=aaaa;
                 end;
-                x=unique(tag_sel);%不同元素个数
+                x=unique(tag_sel);
                 y=tag_sel;
-                [m1,n1]=hist(y,x); %m为n在Y中出现的次数
+                [m1,n1]=hist(y,x); 
                 for i=1:1:length(n1)
                     if n1(i)>0
                         AV(n1(i))=m1(i);
                     end;
                     if m1(i)>0
                         EV(n1(i))=1;
-                    end;%EV为对应的1的时隙，EV1为对应的标签数量
+                    end;
                 end;
                 
                 for i=1:1:f
                     if  AV(i)==1
-                        count=count+1;  %记录消除的已知标签数
-                        a=find(tag_sel==i); %找到映射到该时隙的标签下标
+                        count=count+1;  
+                        a=find(tag_sel==i); 
                         if sum(a)>0
                             for l=1:1:length(a)
                                 e=B(a(l));
-                                Num_all=Num_all-1;%一定是已知标签
-                                delete_uns(k+1)=e;%记录该删除的已知标签，下一轮静墨
+                                Num_all=Num_all-1;
+                                delete_uns(k+1)=e;
                                 k=k+1;
                             end;
                         end;
@@ -55,7 +55,7 @@ function[tt]=edfsa_sam(num_c,size_c,Sa,t_id, t_id2, t_inf)
                 delete=[delete_uns(1:k)];
                 delete_re=zeros(1,length(delete));
                 for i=1:1:length(delete)
-                    delete_re(i)=find(B==delete(i));%找出该删除或者静默的标签在A中对应的下标
+                    delete_re(i)=find(B==delete(i));
                 end;
                 B(delete_re(1:k))=[];
                 t2=t2+f*t_id2+count*t_inf;
