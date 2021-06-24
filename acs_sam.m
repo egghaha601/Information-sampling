c61f12c8d982077b66081b8ae1b5f96235ca5937
egghaha601 <83370677+egghaha601@users.noreply.github.com>
2021-06-23 21:50:33 -0700

    Update acs_sam.m

--- acs_sam.m
+++ acs_sam.m
@@ -1,9 +1,9 @@
  function[tt,bit_c,bit_uc]=acs_sam(num_c,size_c,Sa,t_id, t_id2, t_inf)
-             Mguding=num_c*size_c*Sa; %Number of the sampled tags
-             Nguding=num_c*size_c*(1-Sa); %Number of the unsampled tags
+             Mguding=num_c*size_c*Sa;
+             Nguding=num_c*size_c*(1-Sa); 
              bit_com=0;
              bit_uncom=0;
-             sumshu=Mguding+Nguding; %Number of all the candidated tags
+             sumshu=Mguding+Nguding; 
              NumM=Mguding;
              NumN=Nguding;
              M=NumM;
@@ -12,7 +12,7 @@
              t_com=0;
              t_uncom=0;
              for i=1:sumshu
-                 A(i)=i; %Records the tags in the back-end server
+                 A(i)=i; 
              end;
              %---------------
              while(M>0)
@@ -35,7 +35,7 @@
                          Nidresult1(i)=aaaa;
                      end;
                  end;
-                 result1=[idresult1,Nidresult1]; %slot selection of tags
+                 result1=[idresult1,Nidresult1]; 
                  xa=unique(result1);
                  ya=result1;
                  [ma1,na1]=hist(ya,xa); 
@@ -49,7 +49,7 @@
                      if AV1(i)>0
                          a=find(result1==i);
                          if length(a)==1
-                             if A(a)<=Mguding %Record the time slot selected by the sampled tags
+                             if A(a)<=Mguding 
                                  AV(i)=1;
                              end;
                          end;
@@ -59,7 +59,7 @@
                  if w1>0
                      for i=1:1:f
                          if AV(i)==1
-                             tlongjishu=tlongjishu+1; %Record the reproted sampled tags in each round
+                             tlongjishu=tlongjishu+1; 
                              b=find(result1==i);
                              delete_sam(k+1)=b;
                              k=k+1;
@@ -67,7 +67,7 @@
                          end;
                      end;
                      delete=[delete_sam(1:k),];
-                     A(delete(1:length(delete)))=[]; % Tags that have responded will keep silent
+                     A(delete(1:length(delete)))=[]; 
                      num_group=zeros(1,2);
                      rat_group=zeros(1,2);
                      for i=1:1:f
@@ -85,20 +85,20 @@
                      l_c=0;
                      for i=1:1:2
                          if rat_group(i)>0
-                             l_c=l_c+rat_group(i)*log2(rat_group(i)); % Calculate the length of Vc after compressing
+                             l_c=l_c+rat_group(i)*log2(rat_group(i)); 
                          end;
                      end;
                      l_c=-l_c;
                      t_uncomsub=(f/96)*t_id+(tlongjishu)*t_inf;
-                     t_comsub=(((1*l_c)*f)/96)*t_id+(tlongjishu)*t_inf; %Calculate the execution time of each round 
-                     bit_com=bit_com+ceil(l_c*f); %Calculate the transmitted bits by the reader of each round 
+                     t_comsub=(((1*l_c)*f)/96)*t_id+(tlongjishu)*t_inf;  
+                     bit_com=bit_com+ceil(l_c*f); 
                      bit_uncom=bit_uncom+f;        
                      t_com=t_com+t_comsub;
                      t_uncom=t_uncom+t_uncomsub;
                      M=NumM;
                  end;
              end;
-             tt=t_com./1000000;%compressed
-             tt2=t_uncom./1000000; %uncompressed
+             tt=t_com./1000000;
+             tt2=t_uncom./1000000; 
              bit_c=bit_com;
              bit_uc=bit_uncom;
